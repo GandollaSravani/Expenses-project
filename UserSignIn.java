5ec975e1227f2a96a644f91f3c64e2a9b785aad3
GandollaSravani <147409419+GandollaSravani@users.noreply.github.com>
2023-11-08 15:15:34 +0530

    UserSignIn

---
+++ UserSignIn.java
@@ -0,0 +1,27 @@
+package java.com.srav;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/UserSignInServlet")
+public class UserSignInServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UserSignInServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
